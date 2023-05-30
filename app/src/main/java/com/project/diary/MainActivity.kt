3839093c7028b1089d6496566e036684a2dfef67
MinseoKangQ <kms02171@naver.com>
2023-05-30 16:49:16 +0900

    로그인 화면 디자인 수정

--- app/src/main/java/com/project/diary/MainActivity.kt
+++ app/src/main/java/com/project/diary/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
