95ba06b59a8ac8eca69d266b5b7476ad883f98c8
MinseoKangQ <kms02171@naver.com>
2023-06-22 15:02:15 +0900

    사용하는 캘린더 라이브러리 변경을 위한 되돌림

--- app/src/main/java/com/project/diary/CalendarActivity.kt
+++ app/src/main/java/com/project/diary/CalendarActivity.kt
@@ -2,18 +2,12 @@ package com.project.diary
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.prolificinteractive.materialcalendarview.CalendarDay
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_calendar)
-
-        val materialCalendarView = findViewById<MaterialCalendarView>(R.id.CustomCalendarView)
-
-
     }
 
 }
